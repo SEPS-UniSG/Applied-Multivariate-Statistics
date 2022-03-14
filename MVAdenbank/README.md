9e2fec9c65927ce7fe5d8dc1994d594f082e20e8
LMGV <gorkunvoevoda@mail.ru>
2022-03-14 12:06:13 +0100

    .

---
+++ MVAdenbank/README.md
@@ -0,0 +1,29 @@
+# MVAdenbank
+Computes kernel density estimates of the diagonal of the genuine and forged swiss bank notes. The bandwidth parameter are chosen by Silverman rule of thumb.
+
+```python
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from KDEpy import FFTKDE
+
+# load data
+x = pd.read_csv("bank2.dat", sep = "\s+", header=None)
+x1 = x.iloc[0:100, 5]
+x2 = x.iloc[100:200, 5]
+
+# Compute kernel density estimate
+fh1_x, fh1_y = FFTKDE(bw="silverman", kernel='biweight').fit(np.array(x1)).evaluate()
+fh2_x, fh2_y = FFTKDE(bw="silverman", kernel='biweight').fit(np.array(x2)).evaluate()
+
+fig, ax = plt.subplots(figsize=(10,10))
+ax.plot(fh1_x, fh1_y, c="black")
+ax.plot(fh2_x, fh2_y, c="red", ls=":")
+plt.xlim(137, 143)
+plt.xlabel("Counterfeit                             /                                   Genuine")
+plt.ylabel("Density estimates for diagonals")
+plt.title("Swiss bank notes")
+
+plt.show()
+```
+![MVAdenbank](MVAdenbank_python.png)
---
+++ MVAdenbank3/MVAdenbank3.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from KDEpy import FFTKDE
+from scipy import stats
+
+xx = pd.read_csv("bank2.dat", sep = "\s+", header=None)
+
+dj_xy, dj_f = FFTKDE(bw = 1.06 * np.array([xx[3].std(), xx[4].std()]) * 200**(-1/5), 
+                    kernel='gaussian').fit(np.array(xx.iloc[:,3:5])).evaluate((51, 51))
+d1_x, d1_y = FFTKDE(bw=0.3, kernel='gaussian').fit(np.array(xx[3])).evaluate(51)
+d2_x, d2_y = FFTKDE(bw=0.3, kernel='gaussian').fit(np.array(xx[4])).evaluate(51)
+dp = np.dot(d1_y.reshape(len(d1_y),1), d2_y.reshape(1, len(d2_y)))
+
+X, Y = np.meshgrid(d1_x, d2_x)
+
+fig = plt.figure(figsize=(10, 10))
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(X, Y, dp.T)
+#ax.scatter(x456.iloc[100:, 0], x456.iloc[100:, 1], x456.iloc[100:, 2], c = "w", edgecolors = "r", marker = "^", s = 25)
+#ax.set_xlim(6, 14)
+#ax.set_ylim(6, 14)
+#ax.set_zlim(137, 142)
+ax.view_init(elev=0, azim=0)
+plt.show()
+
+fig = plt.figure(figsize=(10, 10))
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(dj_xy[:,0], dj_xy[:,1], np.reshape(dj_f, (2601,1)))
+#ax.scatter(x456.iloc[100:, 0], x456.iloc[100:, 1], x456.iloc[100:, 2], c = "w", edgecolors = "r", marker = "^", s = 25)
+#ax.set_xlim(6, 14)
+#ax.set_ylim(6, 14)
+#ax.set_zlim(137, 142)
+#ax.view_init(elev=0, azim=0)
+plt.show()
+
+fig, ax = plt.subplots()
+ax.plot(d1_x, d2_x)
+plt.show()
+
