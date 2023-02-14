2d0df2d501d8995448c70ff3450941f782419018
Tim Dass <timdass@gmx.net>
2023-02-14 16:07:32 +0100

    pcp1-ageIncome

---
+++ MVAageIncome/MVAageIncome.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 14 15:36:21 2023
+
+@author: timda
+"""
+
---
+++ MVAageIncome/testing.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 14 15:51:32 2023
+
+@author: timda
+"""
+
