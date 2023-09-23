6931c252b4dc1c5650caebb4b2bfe87916c0bbb7
Mila M G-V <57717914+LMGV@users.noreply.github.com>
2023-09-23 13:25:45 +0200

    Update MVAlinregbh.py

--- MVAlinregbh/MVAlinregbh.py
+++ MVAlinregbh/MVAlinregbh.py
@@ -28,4 +28,4 @@ table = pd.DataFrame({'β-hat': model.params,
                       'p-value': model.pvalues})
 
 table.index = ['Constant'] + [f'X{i}' for i in range(1, 14)]
-print(table)
+print(np.round(table, 4))
