faa2e6e5eed1f4bb7755eb5080a2315261ccfc4d
Tim Dass <timdass@gmx.net>
2024-01-12 17:04:23 +0100

    MVAgrouplasso-MVAnpcahousi

--- MVAgrouplasso/MVAgrouplasso.py
+++ MVAgrouplasso/MVAgrouplasso.py
@@ -1,25 +1,39 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn import linear_model
-from sklearn import preprocessing
-from sklearn.datasets import fetch_openml
 from group_lasso import LogisticGroupLasso
 
-# Load the data
+
 splice = pd.read_csv('splice.csv')
 X = splice.iloc[:,1:8]
 y = splice.iloc[:,0]
-
 X_category = pd.get_dummies(X, dtype=int)
-#lambdas = np.linspace(80,0,100)
-groups = np.repeat([1,2,3,4],7)
-#lmb = 0.05
+
+lambdas = np.linspace(0,0.05,20)
+groups = np.repeat([1,2,3,4,5,6,7],4)
+path = np.zeros((len(lambdas), X_category.shape[1]))
+
+fig,ax = plt.subplots(figsize=(8,6))
+
+for i, lmb in enumerate(lambdas):
+    gl = LogisticGroupLasso(groups= groups, 
+                            l1_reg= lmb, 
+                            tol=1e-3,
+                            scale_reg=None, 
+                            supress_warning=True).fit(X_category,y)
+    coefs = gl.coef_
+    path[i, :] = coefs.T[0]
+
+for i in range(path.shape[1]):
+    ax.plot(lambdas,path[:,i])
+
+ax.set_xlabel('Lambda')
+ax.set_ylabel('Coefficients')
+ax.invert_xaxis()
+ax.set_title('Coefficient paths')
+plt.show()
 
 
 
-#for lmb in lambdas:
-    #gl = LogisticGroupLasso(groups= groups, l1_reg= lmb).fit(X_category,y)
-#glc = linear_model.LogisticRegression().fit(X,y)
 
 
